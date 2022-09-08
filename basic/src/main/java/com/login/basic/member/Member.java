dda812f00f1dbdaa29b9ed392a30114792cf493e
keemsora <itsyoursora@gmail.com>
2022-09-08 14:43:59 +0900

    feat: Member 엔티티 추가

---
+++ basic/src/main/java/com/login/basic/member/Member.java
@@ -0,0 +1,25 @@
+package com.login.basic.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+}
